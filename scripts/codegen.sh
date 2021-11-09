d66a35d8987884393e531ecb826a7b9726d23128
Philipp Fruck <dev@p-fruck.de>
2021-11-09 11:45:44 +0100

    introduce codegen
    Co-authored-by: Pius Walter <pius.walter@gmx.net>

---
+++ scripts/codegen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# This simple bash script utilizes the swagger-codegen container to generate an
+# API-client from the backend documentation.
+#
+local_dir="$(dirname $0)"
+
+if command -v podman >/dev/null 2>&1
+then
+  cmd="podman run --security-opt label=disable"
+else
+  cmd="docker run"
+fi
+
+$cmd --rm --network host \
+  -v "${local_dir}":/local \
+  -v "${local_dir}/../lib/ng-api-client":/out \
+  swaggerapi/swagger-codegen-cli-v3 generate \
+    -i http://localhost:8000/openapi.json \
+    -l typescript-angular \
+    -o /out \
+    -c /local/options.json
