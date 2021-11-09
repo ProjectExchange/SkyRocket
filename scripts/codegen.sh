7130ff549fb7ccdc410e466c54b6ec4a2c39ce0d
Pius Walter <pius.walter@gmx.net>
2021-11-09 12:30:10 +0100

    Updated codegen.sh
    Co-authored-by: Philipp Fruck <dev@p-fruck.de>

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -3,7 +3,7 @@
 # This simple bash script utilizes the swagger-codegen container to generate an
 # API-client from the backend documentation.
 #
-local_dir="$(dirname $0)"
+local_dir="$(readlink -f $(dirname $0))"
 
 if command -v podman >/dev/null 2>&1
 then
@@ -16,7 +16,7 @@ $cmd --rm --network host \
   -v "${local_dir}":/local \
   -v "${local_dir}/../lib/ng-api-client":/out \
   swaggerapi/swagger-codegen-cli-v3 generate \
-    -i http://localhost:8000/openapi.json \
+    -i http://localhost:8000/v1/openapi.json \
     -l typescript-angular \
     -o /out \
     -c /local/options.json
