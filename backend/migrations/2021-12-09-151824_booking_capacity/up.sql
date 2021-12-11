e755d8e6162588883cb2a33fa84f98c034742a66
Pius Walter <pius.walter@gmx.net>
2021-12-11 17:11:41 +0100

    Fixed occupancy view
    Co-authored-by: Philipp Fruck <dev@p-fruck.de>

--- backend/migrations/2021-12-09-151824_booking_capacity/up.sql
+++ backend/migrations/2021-12-09-151824_booking_capacity/up.sql
@@ -1,9 +1,9 @@
 CREATE VIEW flights_offers_with_occupancy AS
 SELECT
-    fo.*, SUM(bookings.seats) as occupied,
-    (SELECT flights.departure_icao FROM flights WHERE flights.offer_id=fo.id ORDER BY departure_time ASC LIMIT 1) AS departure_icao,
-    (SELECT flights.arrival_icao FROM flights WHERE flights.offer_id=fo.id ORDER BY arrival_time DESC LIMIT 1) AS arrival_icao
-FROM
-    flights_offers AS fo, bookings
-WHERE bookings.offer_id = fo.id
+    fo.*,
+    COALESCE(sum(bookings.seats), 0) AS occupied,
+    COALESCE((SELECT flights.departure_icao FROM flights WHERE flights.offer_id = fo.id ORDER BY flights.departure_time LIMIT 1), '') AS departure_icao,
+    COALESCE((SELECT flights.arrival_icao FROM flights WHERE flights.offer_id = fo.id ORDER BY flights.arrival_time DESC LIMIT 1), '') AS arrival_icao
+FROM flights_offers AS fo    
+LEFT JOIN bookings ON bookings.offer_id = fo.id
 GROUP BY fo.id
