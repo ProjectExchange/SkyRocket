586c0e04dc47b22687b1213f1c9c2bb0686bf522
Pius Walter <pius.walter@gmx.net>
2021-12-11 17:13:22 +0100

    Fixed typo
    Co-authored-by: Philipp Fruck <dev@p-fruck.de>

--- backend/migrations/2021-12-09-151824_booking_capacity/up.sql
+++ backend/migrations/2021-12-09-151824_booking_capacity/up.sql
@@ -4,6 +4,6 @@ SELECT
     COALESCE(sum(bookings.seats), 0) AS occupied,
     COALESCE((SELECT flights.departure_icao FROM flights WHERE flights.offer_id = fo.id ORDER BY flights.departure_time LIMIT 1), '') AS departure_icao,
     COALESCE((SELECT flights.arrival_icao FROM flights WHERE flights.offer_id = fo.id ORDER BY flights.arrival_time DESC LIMIT 1), '') AS arrival_icao
-FROM flights_offers AS fo    
+FROM flights_offers AS fo
 LEFT JOIN bookings ON bookings.offer_id = fo.id
 GROUP BY fo.id
